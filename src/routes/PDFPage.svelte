3dc22d1c70e391df2101a2e32e8dd006c918766d
TheBlankness <iddinishak@gmail.com>
2024-02-23 17:09:12 +0800

    the the really final commit

--- src/routes/PDFPage.svelte
+++ src/routes/PDFPage.svelte
@@ -1,7 +1,7 @@
 <script>
-	import { onMount, onDestroy, createEventDispatcher } from 'svelte';
+	import { onMount, onDestroy, createEventDispatcher, afterUpdate } from 'svelte';
 	export let page;
-
+	export let zoom;
 	// Create a writable store for the coordinates
 	const dispatch = createEventDispatcher();
 	let canvas;
@@ -9,6 +9,7 @@
 	let height;
 	let clientWidth;
 	let mounted;
+	let prevZoom = null;
 	function measure() {
 		dispatch('measure', {
 			scale: canvas.clientWidth / width
@@ -17,7 +18,7 @@
 	async function render() {
 		const _page = await page;
 		const context = canvas.getContext('2d');
-		const viewport = _page.getViewport({ scale: 1, rotation: 0 });
+		const viewport = _page.getViewport({ scale: zoom, rotation: 0 });
 		width = viewport.width;
 		height = viewport.height;
 		await _page.render({
@@ -27,12 +28,25 @@
 		measure();
 		window.addEventListener('resize', measure);
 	}
-	onMount(render);
+	// onMount(render);
+	afterUpdate(() => {
+		if (zoom !== prevZoom) {
+			// Check if zoom value has changed
+			render();
+			prevZoom = zoom; // Update prevZoom to current zoom value
+		}
+	});
 	onDestroy(() => {
 		window.removeEventListener('resize', measure);
 	});
 </script>
 
-<div>
-	<canvas bind:this={canvas} class="max-w-full" style="width: {width}px;" {width} {height} />
+<div class="">
+	<canvas
+		bind:this={canvas}
+		class={zoom === 1 ? 'max-w-full' : ''}
+		style="width: {width}px;"
+		{width}
+		{height}
+	/>
 </div>
